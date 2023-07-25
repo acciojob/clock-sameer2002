eb91458a343d2883f4767c00e8f657674ea27e26
sameer sagar <60567209+sameer2002@users.noreply.github.com>
2023-07-25 15:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function showTime() {
+	let timer = document.getElementById('timer');
+const currentTime = new Date();
+const date = currentTime.getDate();
+const month = currentTime.getMonth()+1;
+const year = currentTime.getFullYear();
+let hours = currentTime.getHours();
+const minutes = currentTime.getMinutes();
+const seconds = currentTime.getSeconds();
+let dayLevel = "AM"
+if(hours>12){
+	hours-=12;
+	dayLevel = "PM"
+}	
+ let text = `${month}/${date}/${year}, ${hours}:${minutes}:${seconds} ${dayLevel}`;
+timer.innerText = text;
+}
+setInterval(showTime,1000);
